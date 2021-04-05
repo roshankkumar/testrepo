7d2dee92f97880cc90f4b41921ec8ad25bc6e334
roshankkumar <31183030+roshankkumar@users.noreply.github.com>
2021-04-05 13:16:46 +0530

    thirs push

--- ConstantList.java
+++ ConstantList.java
@@ -1,5 +1,5 @@
 class ConstantList {
-	private static final scopes = "scope";
-	private static final entity = "entity";
 	private static final test = "test";
+	private static final mng_scopes = "scope";
+	private static final mng_entity = "entity";
 }
